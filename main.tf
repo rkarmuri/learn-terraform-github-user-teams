d98edcc0e67952269762eb4dc4f677037085c429
Raju Karmuri <rajukarmuri@Rajus-Air.lan>
2023-05-22 15:09:17 -0500

    Testing terraform for github workflow

--- main.tf
+++ main.tf
@@ -1,7 +1,10 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-provider "github" {}
+provider "github" {
+token = "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"
+organization = "test-oss-terraform"
+}
 
 # Retrieve information about the currently (PAT) authenticated user
 data "github_user" "self" {
--- versions.tf
+++ versions.tf
@@ -9,5 +9,5 @@ terraform {
     }
   }
 
-  required_version = "~> 1.0.5"
+  required_version = "~> 1.4.6"
 }
